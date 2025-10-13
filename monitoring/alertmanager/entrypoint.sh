d67337e14220bdf04c3483e2bfcfc78c9225175b
LicoriceAlex <aleksandrsavin03@mail.ru>
2025-10-13 22:00:48 +0500

    added comments

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
 set -e
+# генерим финальный конфиг из шаблона с подстановкой env
 envsubst < /etc/alertmanager/alertmanager.tmpl.yml > /etc/alertmanager/alertmanager.yml
+# запускаем alertmanager с нашим конфигом
 exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
